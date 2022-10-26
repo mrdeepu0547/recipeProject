bdb977435d08bd6b070d258e8090d393b6e95e65
mrdeepu0547 <saideepak.sabbineni2000@gmail.com>
2022-10-26 15:24:29 +0530

    selected recipie

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe-details/recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -11,5 +12,5 @@ export class RecipesComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  selectedRecipe : Recipe;
 }
