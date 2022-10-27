c0ebf276b87cf509727df63270fe350fca3199a8
mrdeepu0547 <saideepak.sabbineni2000@gmail.com>
2022-10-27 16:42:25 +0530

    manage recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe-details/recipe.model';
+import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,13 +8,11 @@ import { Recipe } from '../recipe-details/recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpW3QiSWw9KfBEH_JnfEkepE3VQavLQOICwQ&usqp=CAU'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpW3QiSWw9KfBEH_JnfEkepE3VQavLQOICwQ&usqp=CAU')
-  ];
-  constructor() { }
+  recipes: Recipe[] = [];
+  constructor( private recipeService:RecipeService){}
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.recipes=this.recipeService.getRecipe();
   }
   onRecipeSelected(recipe:Recipe){
     this.recipeWasSelected.emit(recipe);
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,10 @@
+import { Recipe } from "./recipe-details/recipe.model";
 export class RecipeService{
-  
+  private   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpW3QiSWw9KfBEH_JnfEkepE3VQavLQOICwQ&usqp=CAU'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpW3QiSWw9KfBEH_JnfEkepE3VQavLQOICwQ&usqp=CAU')
+  ];
+  getRecipe(){
+    return this.recipes.slice();
+  }
 }
