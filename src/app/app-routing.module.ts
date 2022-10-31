13836776a01a3560f702994bc9cc1128320b0be7
mrdeepu0547 <saideepak.sabbineni2000@gmail.com>
2022-10-31 14:43:20 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'/recipe',component:RecipesComponent},
+  {path:'/shoplist',component:ShoppingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
