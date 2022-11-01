dd64a857f05e414294d313b845459e4c61b4a381
mrdeepu0547 <saideepak.sabbineni2000@gmail.com>
2022-11-01 15:37:46 +0530

    pagereload

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -10,10 +10,6 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
- @Output() featureSelected = new EventEmitter<string>();
-  onSelect(feature:string){
-    this.featureSelected.emit(feature);
   }
 
 }
